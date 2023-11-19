dc9bcae73520d2d52d4f4f5c6f4ac9bfab430ee1
kyusikko <kokyusik91@naver.com>
2023-11-19 17:32:11 +0900

    ✨ SSR 파일로 서버에서 html을 그려주는 파일

---
+++ src/ssr.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { App, PostList } from './component.js';
+
+import { forceWait } from './utils/index.js';
+
+const fetchData = async () => {
+  // await forceWait(10000);
+  const result = (await axios.get('https://jsonplaceholder.typicode.com/posts'))
+    .data;
+  return result;
+};
+
+// 최초 서버에서 그려주는 html 페이지
+export const generateHTML = async ({ todoItems }) => {
+  console.log(todoItems);
+  const data = await fetchData();
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Todo List</title>
+  </head>
+  <body>
+    <div id="app">
+      <h1>서버에서 처음에 렌더링한 투두 html임 💻</h1>
+      ${App(todoItems)}
+    </div>
+    <div>
+      <h1>서버에서 fetch후 렌더링한 html임 💻</h1>
+      ${PostList(data)}
+    </div>
+    <script>window.__INITIAL_MODEL__ = ${JSON.stringify({ todoItems })}</script>
+    <script src="./src/main.js" type="module"></script>
+  </body>
+  </html>
+  `;
+};
